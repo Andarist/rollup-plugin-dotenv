de5f3603e0c69a709281ac25c1bb0ebc871c7448
Kim In Seop <opnayn@gmail.com>
2020-05-22 06:31:01 +0900

    Try to also load `.env.local` file (#2)

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ export default function dotenvPlugin(inputOptions) {
       )([
         `.env.${process.env[envKey]}.local`,
         `.env.${process.env[envKey]}`,
+        '.env.local',
         '.env',
       ]),
     ),
