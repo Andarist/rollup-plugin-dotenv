b6d21bd6f239215deb11a1cd25884534ab761810
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-10-27 22:39:13 +0200

    Update devDependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,9 @@
 import babel from '@rollup/plugin-babel'
-import builtinModules from 'builtin-modules'
 import pkg from './package.json'
-
-const makeExternalPredicate = (externalsArr) => {
-  if (externalsArr.length === 0) {
-    return () => false
-  }
-  const externalPattern = new RegExp(`^(${externalsArr.join('|')})($|/)`)
-  return (id) => externalPattern.test(id)
-}
 
 export default {
   input: 'src/index.js',
   output: { file: pkg.main, format: 'cjs', exports: 'named' },
-  external: makeExternalPredicate([
-    ...builtinModules,
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ]),
+  external: (id) => !/^(\.|\/)/.test(id),
   plugins: [babel({ babelHelpers: 'bundled' })],
 }
