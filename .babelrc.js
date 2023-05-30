0615ce68666874a03bb4e274383bae4180cd24f4
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-05-30 09:28:09 +0200

    Use Preconstruct for bundling

--- .babelrc.js
+++ .babelrc.js
@@ -1,14 +1,8 @@
-const { BABEL_ENV, NODE_ENV } = process.env
-const modules = BABEL_ENV === 'cjs' || NODE_ENV === 'test' ? 'commonjs' : false
-const loose = true
-
 module.exports = {
   presets: [
     [
       '@babel/env',
       {
-        loose,
-        modules,
         targets: { node: 14 },
       },
     ],
